5731d0dd9f29f962302a6f0f0d536d6c686eadae
Verónica Ojeda <glebapps@gmail.com>
2021-05-06 20:33:57 +0200

    add search icon to search box

--- components/Input/Search.vue
+++ components/Input/Search.vue
@@ -1,11 +1,34 @@
 <template>
-  <div>
+  <div class="input">
+    <SearchIcon class="input__icon" />
     <input type="text" />
   </div>
 </template>
 
+<script>
+import SearchIcon from '~/assets/svg/search.svg?inline'
+export default {
+  components: {
+    SearchIcon,
+  },
+}
+</script>
+
 <style lang="scss" scoped>
-input {
-  @include inputStyle(16px);
+.input {
+  position: relative;
+  display: flex;
+  align-items: center;
+
+  &__icon {
+    position: absolute;
+    left: 4px;
+  }
+
+  input {
+    @include inputStyle(16px);
+
+    padding-left: 32px;
+  }
 }
 </style>
