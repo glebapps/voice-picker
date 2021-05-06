88de74ce92c6857a7c5ff3ed893034c2f4854436
Verónica Ojeda <glebapps@gmail.com>
2021-05-06 20:05:04 +0200

    add missing stylelint config

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,6 +1,16 @@
 module.exports = {
-  extends: ['stylelint-config-standard', 'stylelint-config-prettier'],
+  extends: [
+    'stylelint-config-standard',
+    'stylelint-config-rational-order',
+    'stylelint-config-prettier',
+  ],
   // add your custom config here
   // https://stylelint.io/user-guide/configuration
-  rules: {},
+  plugins: ['stylelint-scss', 'stylelint-prettier'],
+  rules: {
+    'prettier/prettier': true,
+    'at-rule-no-unknown': null,
+    'scss/at-rule-no-unknown': true,
+    'no-descending-specificity': null,
+  },
 }
