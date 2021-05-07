b5a5c75e24d08461dc09235c08ae4b6b52e16166
Verónica Ojeda <glebapps@gmail.com>
2021-05-07 16:53:40 +0200

    add random button

---
+++ components/RandomButton.vue
@@ -0,0 +1,12 @@
+<template>
+  <RandomIcon />
+</template>
+
+<script>
+import RandomIcon from '~/assets/svg/button-random.svg?inline'
+export default {
+  components: {
+    RandomIcon,
+  },
+}
+</script>
