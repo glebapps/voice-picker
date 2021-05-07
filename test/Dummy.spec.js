6ca8350048c6989e8ef256632faf833ac1cce758
Verónica Ojeda <glebapps@gmail.com>
2021-05-08 01:00:57 +0200

    fix dummy test

--- test/Dummy.spec.js
+++ test/Dummy.spec.js
@@ -1,9 +1,9 @@
 import { mount } from '@vue/test-utils'
-import Logo from '@/components/Logo.vue'
+import Toolbar from '@/components/Toolbar.vue'
 
-describe('Logo', () => {
+describe('Dummy', () => {
   test('is a Vue instance', () => {
-    const wrapper = mount(Logo)
+    const wrapper = mount(Toolbar)
     expect(wrapper.vm).toBeTruthy()
   })
 })
