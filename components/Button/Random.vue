8c3be26a304c33f713d1804541980cf880c2c26e
Verónica Ojeda <glebapps@gmail.com>
2021-05-09 20:39:53 +0200

    improve random a11y and animate it

--- components/Button/Random.vue
+++ components/Button/Random.vue
@@ -1,7 +1,15 @@
 <template>
-  <div @click="pickRandomVoice">
-    <RandomPickerIcon />
-  </div>
+  <button
+    aria-label="Choose a random voice"
+    title="Choose a random voice"
+    @click="pickRandomVoice"
+  >
+    <RandomPickerIcon
+      class="random-icon"
+      aria-hidden="true"
+      role="presentation"
+    />
+  </button>
 </template>
 
 <script>
@@ -13,6 +21,14 @@ export default {
   methods: {
     pickRandomVoice() {
       this.$store.commit('pickRandomVoice')
+      this.scrollToSelected()
+    },
+    scrollToSelected() {
+      const selectedVoiceId = this.$store.state.selectedVoice
+      const selectedVoiceEl = document.getElementById(selectedVoiceId)
+      selectedVoiceEl.scrollIntoView({
+        behavior: 'smooth',
+      })
     },
   },
 }
@@ -23,7 +39,21 @@ button {
   @include buttonStyle();
 }
 
-div {
+.random-icon {
   cursor: pointer;
+
+  &:hover {
+    animation-name: spin;
+    animation-duration: 0.5s;
+    animation-delay: 0.2s;
+    @keyframes spin {
+      from {
+        transform: rotate(0deg);
+      }
+      to {
+        transform: rotate(360deg);
+      }
+    }
+  }
 }
 </style>
